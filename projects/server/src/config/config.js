8ce8745de4c1ef4e6bdd7855186932e161024eb3
Ryan Defryan <muhammaddefryan@gmail.com>
2023-07-18 22:31:48 +0700

    Update config.js

--- projects/server/src/config/config.js
+++ projects/server/src/config/config.js
@@ -3,23 +3,23 @@ require('dotenv').config({ path: join(__dirname, '../.env') });
 
 module.exports = {
   development: {
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
+    username: 'jcwdol0905',
+    password: 'groupol0905',
+    database: 'jcwdol0905',
     host: "127.0.0.1",
     dialect: "mysql",
   },
   test: {
-    username: "root",
-    password: null,
-    database: "database_test",
+    username: 'jcwdol0905',
+    password: 'groupol0905',
+    database: 'jcwdol0905',
     host: "127.0.0.1",
     dialect: "mysql",
   },
   production: {
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
+    username: 'jcwdol0905',
+    password: 'groupol0905',
+    database: 'jcwdol0905',
     host: "127.0.0.1",
     dialect: "mysql",
   },
