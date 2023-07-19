5aef1a9755b11acf2eb00eaf1c4363cc495c7128
Ryan Defryan <muhammaddefryan@gmail.com>
2023-07-19 21:52:58 +0700

    Update auth.js

--- projects/server/src/middleware/auth.js
+++ projects/server/src/middleware/auth.js
@@ -3,6 +3,7 @@ require("dotenv").config({ path: join(__dirname, "../.env") });
 const key = "apahayo"
 
 module.exports = (req, res, next) => {
+    console.log('Headers Secret Key')
     console.log(req.headers.secret_key)
     console.log(key)
     if (req.headers.secret_key == key) {
