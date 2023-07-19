0cbe4ba09d2e92b65835b5a04d2ce9a44e077306
Ryan Defryan <muhammaddefryan@gmail.com>
2023-07-19 21:55:56 +0700

    Update auth.js

--- projects/server/src/middleware/auth.js
+++ projects/server/src/middleware/auth.js
@@ -1,5 +1,4 @@
-const { join } = require("path");
-require("dotenv").config({ path: join(__dirname, "../.env") });
+require("dotenv/config");
 const key = "apahayo"
 
 module.exports = (req, res, next) => {
